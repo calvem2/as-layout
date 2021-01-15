063374ae47781bbbcf3f19a80494d993b5ca2f19
Jennifer Mankoff <jmankoff@cs.washington.edu>
2021-01-15 10:40:00 -0800

    Modifications to part3

--- app/src/main/java/cse340/layout/Part3View.java
+++ app/src/main/java/cse340/layout/Part3View.java
@@ -2,16 +2,18 @@ package cse340.layout;
 
 import android.content.Context;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.ScrollView;
 
-import java.util.Collections;
 import java.util.List;
 
 public class Part3View extends ScrollView {
 
-    public Part3View(Context context) {
-        this(context, Collections.emptyList(), 0);
-    }
+    /** The image views to layout out */
+    protected List<ImageView> mImageViews;
+
+    /** The margin below each image */
+    int mVMargin;
 
     /**
      * We want to create a Pinterest-link image flow container. This part requires both XML
@@ -20,7 +22,7 @@ public class Part3View extends ScrollView {
      * Inflate a ConstraintLayout from part3_grid.xml, which should contain two LinearLayouts inside to evenly divide
      * the width. Remember to add the left/right LinearLayouts to the part3_grid.xml file before
      * proceeding with the remainder of this part.
-     * <p>
+     *
      * When the view is rotated, the columns should change width to maintain the 50%
      * split. Add each image into one of the LinearLayout columns. Track the "height" of each column
      * and insert the next image in the column with the lower height, or leftmost one if equal.
@@ -38,35 +40,54 @@ public class Part3View extends ScrollView {
      * Each image should expand to fill the width as constrained by the margins described above. The
      * height should be such that the aspect ratio matches the source drawable.
      *
-     * @param images  List of ImageViews to put in the View
+     * @param images List of drawable names, e.g. "animal_0", to put in the view.
      * @param vMargin Base vertical margin for function in px. Default is 16dp; this is defined in dimens.xml
      */
     public Part3View(Context context, List<ImageView> images, int vMargin) {
 
         super(context);
 
+        mImageViews = images;
+        mVMargin = vMargin;
+
         // TODO Design part3_grid.xml
         // TODO Inflate R.layout.part3_grid
         // TODO add it to this View
 
+    }
+
+    /**
+     * We are overriding  onMeasure() because at this point enough information exists to lay out our views. We will need to
+     * first empty both columns, then add one view at a time checking the measurements after each addition.
+     *
+     * @param widthMeasureSpec
+     * @param heightMeasureSpec
+     */
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        // Run the measurement code that Android provides to make sure we have a width and height
+        // set up for the columns
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+
         // TODO Get references to the left and right columns (LinearLayouts specified in part3_grid)
         // TODO You can use R.id for this if you set up their ID properties properly in part3_grid
+        // Then remove all the views in case anything has changed...
+        
+        int leftHeight = 0, rightHeight = 0;
 
+        // TODO Setup layout params for the images up since we'll be using the same params overr and over
+        // Note: This is much simpler than for ConstraintLayout, it does a lot for you. Don't forget to include the margin!
 
-        // TODO get access to res so you can find the image files
-        for (int i = 0; i < images.size(); i++) {
-            // TODO Use LinearLayout.LayoutParams to set the image up since we'll be adding it to a LinearLayout
-            // Note: This is much simpler than for ConstraintLayout, it does a lot for you
-            // TODO: Be sure to set up vMargin properly
-
-            // TODO: Get the image's height
-            // TODO: First you need to ask it to measure itself
-            // TODO: Then you can get it's (measured) height
-
+        // Loop through the views
+        for (ImageView img : mImageViews) {
+            // TODO create a new image and set it up (see Part2View for how to do this)
             // TODO: Figure out which column to add it to based on the current column heights
-            // TODO update the appropriate height
+            // TODO also update the appropriate column height
 
-            // TODO: Apply the layout params after the image has been added
         }
+
+        // let the toolkit confirm measurement properly
+        // once all the images are added.
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
     }
 }
