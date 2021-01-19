7fb2f71537f88309a6ce7aeff59726f8cd821b2f
Megan <calvem2@cs.washington.edu>
2021-01-19 12:14:48 -0800

    part 3 ish idk it's a mess.

--- app/src/main/java/cse340/layout/Part3View.java
+++ app/src/main/java/cse340/layout/Part3View.java
@@ -1,11 +1,23 @@
 package cse340.layout;
 
 import android.content.Context;
+import android.support.constraint.ConstraintLayout;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.ScrollView;
 
 import java.util.Collections;
 import java.util.List;
+
+// Documentation used
+// LayoutInflater: https://developer.android.com/reference/android/view/LayoutInflater.html
+// Lecture: https://courses.cs.washington.edu/courses/cse340/21wi/slides/wk02/layout.html.pdf
+// LayoutParams: https://developer.android.com/reference/android/widget/LinearLayout.LayoutParams
+// View: https://developer.android.com/reference/android/view/View
+// MeasureSpec: https://developer.android.com/reference/android/view/View.MeasureSpec
 
 public class Part3View extends ScrollView {
 
@@ -48,25 +60,69 @@ public class Part3View extends ScrollView {
         // TODO Design part3_grid.xml
         // TODO Inflate R.layout.part3_grid
         // TODO add it to this View
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View newView = inflater.inflate(R.layout.part3_grid, this);
 
         // TODO Get references to the left and right columns (LinearLayouts specified in part3_grid)
         // TODO You can use R.id for this if you set up their ID properties properly in part3_grid
-
+        LinearLayout left = newView.findViewById(R.id.left);
+        LinearLayout right = newView.findViewById(R.id.right);
+        int leftHeight = left.getMeasuredHeight();
+        int rightHeight = right.getMeasuredHeight();
+        System.out.println(left.getLayoutParams().width);
 
         // TODO get access to res so you can find the image files
         for (int i = 0; i < images.size(); i++) {
+            ImageView img = images.get(i);
             // TODO Use LinearLayout.LayoutParams to set the image up since we'll be adding it to a LinearLayout
             // Note: This is much simpler than for ConstraintLayout, it does a lot for you
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+
             // TODO: Be sure to set up vMargin properly
+            params.setMargins(vMargin, vMargin, vMargin, 0);
 
             // TODO: Get the image's height
             // TODO: First you need to ask it to measure itself
             // TODO: Then you can get it's (measured) height
 
+            // first measure the column ?
+//            newView.measure(MeasureSpec.makeMeasureSpec(0, MeasureSpec.UNSPECIFIED),
+//                    MeasureSpec.makeMeasureSpec(0, MeasureSpec.UNSPECIFIED));
+//
+            left.measure(MeasureSpec.makeMeasureSpec(ConstraintLayout.LayoutParams.MATCH_CONSTRAINT, MeasureSpec.AT_MOST),
+                    MeasureSpec.makeMeasureSpec(0, MeasureSpec.UNSPECIFIED));
+//            right.measure(MeasureSpec.makeMeasureSpec(newView.getMeasuredWidth(), MeasureSpec.AT_MOST),
+//                    MeasureSpec.makeMeasureSpec(0, MeasureSpec.UNSPECIFIED));
+            // the measure the img ?
+//            img.measure(MeasureSpec.makeMeasureSpec(left.getMeasuredWidth(), MeasureSpec.AT_MOST),
+//                    MeasureSpec.makeMeasureSpec(0, MeasureSpec.UNSPECIFIED));
+
+
+//            left.measure(MeasureSpec.makeMeasureSpec(this.getWidth(), MeasureSpec.AT_MOST),
+//                    MeasureSpec.makeMeasureSpec(this.getHeight(), MeasureSpec.UNSPECIFIED));
+//            img.measure(MeasureSpec.makeMeasureSpec(0, MeasureSpec.AT_MOST),
+//                    MeasureSpec.makeMeasureSpec(0, MeasureSpec.UNSPECIFIED));
+
+            System.out.println(newView.getMeasuredWidth() + " " + left.getMeasuredHeight() + " " + left.getMeasuredWidth() + " " + img.getMeasuredHeight());
             // TODO: Figure out which column to add it to based on the current column heights
             // TODO update the appropriate height
+            LinearLayout toAddTo = left.getMeasuredHeight() <= right.getMeasuredHeight() ? left : right;
+//            toAddTo.addView(img);
+//            if (leftHeight <= rightHeight) {
+//                left.addView(img);
+//                params.setMargins(vMargin, vMargin, vMargin / 2, 0);
+//                leftHeight += img.getMeasuredHeight();
+//            } else {
+//                right.addView(img);
+//                params.setMargins(vMargin / 2, vMargin, vMargin, 0);
+//                rightHeight += img.getMeasuredHeight();
+//            }
 
+//            toAddTo.measure(MeasureSpec.makeMeasureSpec(this.getWidth(), MeasureSpec.EXACTLY), MeasureSpec.makeMeasureSpec(this.getHeight(), MeasureSpec.UNSPECIFIED));
+//            toAddTo.measure(MeasureSpec.makeMeasureSpec(ConstraintLayout.LayoutParams.MATCH_CONSTRAINT, MeasureSpec.EXACTLY),
+//                    MeasureSpec.makeMeasureSpec(ConstraintLayout.LayoutParams.MATCH_CONSTRAINT, MeasureSpec.UNSPECIFIED));
             // TODO: Apply the layout params after the image has been added
+            img.setLayoutParams(params);
         }
     }
 }
