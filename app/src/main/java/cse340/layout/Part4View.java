694805b51963c5f8bd0b50b4e379996122a116d2
Megan <calvem2@cs.washington.edu>
2021-01-25 19:25:27 -0800

    Start part 4.

--- app/src/main/java/cse340/layout/Part4View.java
+++ app/src/main/java/cse340/layout/Part4View.java
@@ -2,7 +2,12 @@ package cse340.layout;
 
 import android.content.Context;
 import android.support.constraint.ConstraintLayout;
+import android.view.LayoutInflater;
 
+// Documentation used
+// default search icon: https://stackoverflow.com/questions/47338053/using-android-studio-default-search-icon-for-searchview
+// text vertical align: https://developer.android.com/reference/android/widget/TextView#attr_android:gravity
+// aligning items to left & right of container: https://stackoverflow.com/questions/2099249/aligning-textviews-on-the-left-and-right-edges-in-android-layout
 /**
  * This view should imitate an interface you've seen before and like. If you need ideas
  * about what to imitate, check out this repository: http://interactionmining.org/rico
@@ -12,6 +17,9 @@ public class Part4View extends ConstraintLayout {
     public Part4View(Context context) {
         super(context);
         // TODO: Build your desired layout for part 4. Make use of a combination of XML and programmatic techniques to accomplish this.
+
+        LayoutInflater inflater = LayoutInflater.from(context);
+        inflater.inflate(R.layout.part4, this);
     }
 
 }
